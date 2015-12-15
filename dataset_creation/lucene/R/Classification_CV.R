f406900b844269013efc9035a5c6273fd0f8b302
kajdreef <kaj.dreef@gmail.com>
2015-12-15 17:11:11 +0100

    Added a work in progress for classification with cross-validation

---
+++ dataset_creation/lucene/R/Classification_CV.R
@@ -0,0 +1,67 @@
+###########################################################################################
+# Classification of the data to see if there is a correlation
+#
+# Tutorial: http://scg.sdsu.edu/rf_r/
+# Documentation: http://www.inside-r.org/packages/cran/randomforest/docs/randomforest
+# Info on importance output: https://dinsdalelab.sdsu.edu/metag.stats/code/randomforest.html
+###########################################################################################
+
+# Clear history
+(rm(list=ls()))
+
+
+# Import libraries
+library(dplyr)
+library(ggplot2)
+library(randomForest)
+
+set.seed(71)
+
+lucene_metrics <-read.csv(file="../dataset/lucene_features.csv",head=TRUE,sep=",")
+
+lucene_metrics_selected <- lucene_metrics %>%
+  select( commit_ownership, minor_contributors, major_contributors,
+          line_ownership_added, lines_added_major_contributors, lines_added_minor_contributors,
+          line_ownership_deleted, lines_deleted_major_contributors, lines_deleted_minor_contributors,
+          implicated) %>%
+  transform(implicated = as.factor(implicated))
+
+lucene_bugs <- lucene_metrics_selected %>%
+  filter(implicated == 1)
+
+lucene_non_bugs <- lucene_metrics_selected %>%
+  filter(implicated == 0)
+
+### K-folds
+k = 4
+
+# create the dataset with even number of buggy/non-buggy rows
+sample_data_set1 <- lucene_bugs[sample(nrow(lucene_bugs), 2000, replace=TRUE, prob=NULL), ]
+sample_data_set2 <- lucene_non_bugs[sample(nrow(lucene_non_bugs), 2000, replace=TRUE, prob=NULL), ]
+sample_data <- rbind(sample_data_set1, sample_data_set2)
+
+# Sample 4 folfds of data with replace
+sample_data$id <- sample(1:k, nrow(sample_data), 1)
+
+prediction <- data.frame()
+testsetCopy <- data.frame()
+
+list <- 1:k
+for (i in 1:k){
+  # remove rows with id i from dataframe to create training set
+  # select rows with id i to create test set
+  trainingset <- subset(sample_data, id %in% list[-i]) %>% select(-id)
+  testset <- sample_data %>% filter(id == i) %>% select(-id)
+  
+  # run a random forest model
+  train.rf <- randomForest(implicated ~ ., data=trainingset, importance=TRUE)
+  print(train.rf)
+  round(importance(train.rf), 2)
+  
+  
+  train.rf.pr = predict(train.rf, type="prob", newdata=testset)[,2]
+  train.rf.pred = prediction(train.rf.pr, testset$implicated)
+  train.rf.perf <- performance(train.rf.pred,"tpr","fpr")
+  plot(train.rf.perf,main="ROC Curve for Random Forest",col=2,lwd=2)
+  abline(a=0,b=1,lwd=2,lty=2,col="gray")
+}
